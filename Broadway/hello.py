4fceb7de63a535a77493d7d1781c1b6bfa49bc02
gunjanak <janak.klal@gmail.com>
2025-02-05 20:02:27 +0545

    Streamlit multipage app with page on bank data analysis

---
+++ Broadway/hello.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+st.set_page_config(page_title="My Multi-Page App", layout="wide")
+
+st.title("Welcome to My Multi-Page Streamlit App! 🎈")
+st.write("Use the sidebar to navigate between different pages.")
