f244af5dd823a4945a0a9fdfcc62bebf83a24951
gunjanak <janak.klal@gmail.com>
2025-02-12 23:05:58 +0545

    Page for k-means created

---
+++ Multipage/pages/page5.py
@@ -0,0 +1,10 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+import plotly.express as px
+import plotly.figure_factory as ff
+from scipy.stats import norm,t
+
+st.title("K-means Clustering")
