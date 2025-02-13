44e50201fae29548f830d13c0efa16e63eca2cf7
gunjanak <janak.klal@gmail.com>
2025-02-13 22:24:59 +0545

    readme changed

--- Broadway/hello.py
+++
@@ -1,6 +0,0 @@
-import streamlit as st
-
-st.set_page_config(page_title="My Multi-Page App", layout="wide")
-
-st.title("Welcome to My Multi-Page Streamlit App! 🎈")
-st.write("Use the sidebar to navigate between different pages.")
