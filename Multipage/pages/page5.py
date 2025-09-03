9e042a13a2fdc62d573f0d4cfcdf5b95ba8bd870
gunjanak <janak.klal@gmail.com>
2025-09-03 11:36:28 +0545

    just image uploader

--- Multipage/pages/page5.py
+++ Multipage/pages/page5.py
@@ -6,13 +6,13 @@ st.title("Zero-DCE")
 # File uploader
 uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
 
-if uploaded_file is not None:
-    # Open the image
-    image = Image.open(uploaded_file)
+# if uploaded_file is not None:
+#     # Open the image
+#     image = Image.open(uploaded_file)
 
-    # Display the image
-    st.image(image, caption="Uploaded Image", use_column_width=True)
+#     # Display the image
+#     st.image(image, caption="Uploaded Image", use_column_width=True)
 
-    # Show basic info
-    st.write("Image format:", image.format)
-    st.write("Image size:", image.size)
+#     # Show basic info
+#     st.write("Image format:", image.format)
+#     st.write("Image size:", image.size)
